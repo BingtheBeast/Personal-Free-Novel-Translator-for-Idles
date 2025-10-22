c65f8f95bad5a075a5af870307f4075775903b2d
BingtheBeast <dr.musabahmed1@gmail.com>
2025-10-22 07:36:36 +0400

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -1,5 +1,9 @@
 import { scrapeChapter } from './scrape.js';
 
+export const config = {
+    runtime: 'edge',
+};
+
 export default async function handler(req) {
     if (req.method !== 'POST') return new Response(JSON.stringify({ error: 'Method Not Allowed' }), { status: 405 });
 
